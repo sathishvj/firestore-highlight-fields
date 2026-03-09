2b068d3bb6fabd1916fc14d1b713de4816307052
sathishvj <sathishvj@yahoo.com>
2026-03-09 18:06:40 +0530

    chore: remove unused popup.html and popup.js

--- popup.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const manifestData = chrome.runtime.getManifest();
-  const versionSpan = document.getElementById('version-number');
-  if (versionSpan) {
-    versionSpan.textContent = manifestData.version;
-  }
-});
